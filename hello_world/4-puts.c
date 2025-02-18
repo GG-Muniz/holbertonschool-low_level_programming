1b33894be70adb39475c89d9dd71155c6d56e44f
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-18 09:16:36 -0400

    Fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,6 @@
-#!/bin/bash
-gcc puts.c -o puts
-
 #include <studio.h>
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
