5a538cdbcf85c059e240c762e42da6afbe53e249
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-18 10:14:29 -0400

    Size is not grandeur, and terretory does not make a nation.

---
+++ hello_world/6-size.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+
+int main (void){
+	printf("\"Size of char: 1 byte(s)");
+	printf("\"Size of int: 4 byte(s)");
+	printf("\"Size of a long int: 4 byte(s)");
+	printf("\"Size of a long long int: 8 byte(s)");
+	printf("\"Size of a float: 4 byte(s)");
+	return (0);
+}
