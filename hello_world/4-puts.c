b8461d7e6e8a3a2e016c0918ec2226abc717debf
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-18 11:07:43 -0400

    Fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * Main will print a line
- * Return 0
+ * main - will print a line
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
