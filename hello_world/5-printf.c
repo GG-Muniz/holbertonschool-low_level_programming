398e5111db5cd55a76c96d7e27c991dbf53e1aea
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-18 10:32:44 -0400

    Fix

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -3,10 +3,9 @@
  * Main will print a line
  * 
  * Return 0.
- */
- 
+ */ 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
