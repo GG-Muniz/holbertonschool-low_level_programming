518ca86797f6b9b6b8565e89f99db1077f60b3b9
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-21 11:57:26 -0400

    Fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -4,17 +4,16 @@
  * main - program will print '_putchar' followed by a new line.
  * Return: 0
 */
-
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
