3d27c516b3d0f4c0ab42335bb14675c71d3301f4
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-21 11:04:28 -0400

    Directory

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program will print '_putchar' followed by a new line.
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Description: this directory is to practice and learn funtions and nested loops in C.
