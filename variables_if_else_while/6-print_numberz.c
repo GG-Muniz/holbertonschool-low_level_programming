539a1d56ae3ac369ad717404154c9ad39ae32420
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-21 09:47:19 -0400

    Fix

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -9,9 +9,11 @@
 
 int main(void)
 {
+	/* Declaring variable. */
 	int i;
+	/* Using putchar to print all single digits of base 10 starting from 0.*/
 	for (i = 0; i < 10; i++)
 		putchar(i + '0');
 	putchar('\n');
-	return 0;
+	return (0);
 }
