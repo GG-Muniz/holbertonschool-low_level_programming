c903d45deb6dc1a9ebc8f7400786426e8695aefb
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-21 09:44:08 -0400

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints all digit numbers of base 10 starting from 0.
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return 0;
+}
