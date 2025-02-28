2a55b74cffce51ef8ae46019a38e91732398700f
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-28 11:28:01 -0400

    Fix

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 /**
  * _puts - prints a string followed by a new line.
  * @str: reference to the string to print.
 */
 void _puts(char *str)
 {
-	printf("%s\n", str);
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write (1, "\n", 1);
 }
