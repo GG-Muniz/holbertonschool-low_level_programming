7de1c316889db74887e1815c624172210894722b
GG-Muniz <g.garciamuniz@outlook.com>
2025-02-28 11:00:53 -0400

    This report, by its very length, defends itself against the risk of being read.

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - prints the length of a string.
+ * @s: string used to print the length of.
+ * Return: returns the length of the string.
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
