177766e4227ed16d8eaae071ab1c2d8e12b484db
GG-Muniz <g.garciamuniz@outlook.com>
2025-03-01 13:05:01 -0400

    Directory change/fix

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * strncat - function will concatenate 2 strings.
+ * _strncat - function will concatenate 2 strings.
  * @dest: destination of the string that will get concatenated.
  * @src: source of the other string to concatenate.
  * @n: variable used to count the bytes from src.
