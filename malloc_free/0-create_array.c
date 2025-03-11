bd534c13e2e774cd2f172ce982bac0ecab2b1e64
GG-Muniz <g.garciamuniz@outlook.com>
2025-03-11 15:15:35 -0400

    0 Task push

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * *create_array - creates an array of chars and initializes it with a specific char. 
+ * *create_array - creates an array of chars and initializes it with char.
  * @size: size of array.
  * @c: specific char that initializes array.
  *
@@ -15,12 +16,12 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	array = malloc (sizeof(char) * size);
+	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0, i < size; i ++)
+	for (i = 0; i < size; i++);
 		array[i] = c;
 
 	return (array);
