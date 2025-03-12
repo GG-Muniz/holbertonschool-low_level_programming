86decef6e56eab3a66fadfd83a70b4ec108a56e8
GG-Muniz <g.garciamuniz@outlook.com>
2025-03-12 12:38:41 -0400

    Fix. The malloc return statement was not present.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -29,6 +29,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	concat = malloc(sizeof(char) * (len1 + n + 1));
 
+	if (concat == NULL)
+		return (NULL);
+
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
 	for (j = 0; j < n; j++, i++)
