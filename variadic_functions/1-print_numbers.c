be5f0ebf541e491be8d94d38536f42b729317154
GG-Muniz <g.garciamuniz@outlook.com>
2025-03-22 16:01:59 -0400

    Fix. Missing necessary libraries in the header file, also compiling file.

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,5 +1,5 @@
 #include "variadic_functions.h"
-#include <stdvar.h>
+#include <stdarg.h>
 /**
  * print_numbers - prints numbers followed by a new line.
  * @separator: string to be printed between numbers.
