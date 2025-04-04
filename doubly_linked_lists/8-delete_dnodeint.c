5c836a5efa1910794aa54d13e3da688634225a48
GG-Muniz <g.garciamuniz@outlook.com>
2025-04-04 18:49:17 -0400

    Fixing issue with compilement of task #8. Fixed error in the function where there was an undefined reference made to the function when called.

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,53 +1,52 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: double pointer to the head of the list
- * @idx: index where the new node should be added (starts at 0)
- * @n: value to store in the new node
+ * delete_dnodeint_at_index - deletes the node at a given index
+ * @head: double pointer to the head of the list
+ * @index: index of the node to delete (starts at 0)
  *
- * Return: address of the new node, or NULL if it failed
+ * Return: 1 if succeeded, -1 if failed
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *new_node, *current;
+	dlistint_t *current;
 	unsigned int i = 0;
 
-	/* Return NULL if h is NULL */
-	if (h == NULL)
-		return (NULL);
+	/* Check if list is empty */
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-	/* If index is 0, use add_dnodeint */
-	if (idx == 0)
-		return (add_dnodeint(h, n));
+	/* Start at the beginning of the list */
+	current = *head;
 
-	/* Traverse to the position before the desired index */
-	current = *h;
-	while (current != NULL && i < idx - 1)
+	/* Special case: delete first node (index 0) */
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	/* Traverse list to find the node at index */
+	while (current != NULL && i < index)
 	{
 		current = current->next;
 		i++;
 	}
 
-	/* Return NULL if index is out of bounds */
+	/* If index is out of bounds */
 	if (current == NULL)
-		return (NULL);
+		return (-1);
 
-	/* If inserting at the end, use add_dnodeint_end */
-	if (current->next == NULL)
-		return (add_dnodeint_end(h, n));
+	/* Update pointers of surrounding nodes */
+	current->prev->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
 
-	/* Create new node */
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
+	/* Free the node */
+	free(current);
 
-	/* Set new node values and connect it to the list */
-	new_node->n = n;
-	new_node->prev = current;
-	new_node->next = current->next;
-	current->next->prev = new_node;
-	current->next = new_node;
-
-	return (new_node);
+	return (1);
 }
