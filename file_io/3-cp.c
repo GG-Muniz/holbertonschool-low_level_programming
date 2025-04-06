f4abeddd42b15fc227acdba5dd8faaf5f1fa4f58
GG-Muniz <g.garciamuniz@outlook.com>
2025-04-06 15:51:58 -0400

    Testing fix on return value.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+
 #define BUFFER_SIZE 1024
 #define USAGE "Usage: cp file_from file_to\n"
 #define READ_ERR "Error: Can't read from file %s\n"
 #define WRITE_ERR "Error: Can't write to %s\n"
 #define CLOSE_ERR "Error: Can't close fd %d\n"
+
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
@@ -26,16 +28,20 @@ int main(int argc, char *argv[])
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), close(fd_from), exit(99);
-	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+
+	r = read(fd_from, buffer, BUFFER_SIZE);
+	while (r > 0)
 	{
 		w = write(fd_to, buffer, r);
 		if (w == -1 || w != r)
 			dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), close(fd_from),
 			close(fd_to), exit(99);
+		r = read(fd_from, buffer, BUFFER_SIZE);
 	}
 	if (r == -1)
 		dprintf(STDERR_FILENO, READ_ERR, argv[1]), close(fd_from),
 		close(fd_to), exit(98);
+
 	c = close(fd_from);
 	if (c == -1)
 		dprintf(STDERR_FILENO, CLOSE_ERR, fd_from), exit(100);
